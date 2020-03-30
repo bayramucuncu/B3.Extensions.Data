dce0d11fff711af39a9af75e7b50037418991603
Bayram Üçüncü <ucuncubayram@gmail.com>
2020-03-30 16:35:42 +0300

    Async query execution added.

--- B3.Extensions.Data/DbContextExtensions.cs
+++ B3.Extensions.Data/DbContextExtensions.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Dynamic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace B3.Extensions.Data
@@ -23,6 +24,28 @@ namespace B3.Extensions.Data
                 command.Connection.Open();
 
             using var dataReader = command.ExecuteReader();
+
+            var dataRow = ReadData(dataReader);
+
+            if (command.Connection.State == ConnectionState.Open)
+                command.Connection.Close();
+
+            return dataRow;
+        }
+
+        public static async Task<IEnumerable<Dictionary<string, object>>> ExecuteQueryAsync(this DbContext context, string query, params object[] parameters)
+        {
+            await using var command = context.Database.GetDbConnection().CreateCommand();
+
+            command.CommandText = query;
+
+            if (parameters.Any())
+                command.Parameters.AddRange(parameters);
+
+            if (command.Connection.State != ConnectionState.Open)
+                command.Connection.Open();
+
+            await using var dataReader = await command.ExecuteReaderAsync();
 
             var dataRow = ReadData(dataReader);
 
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # B3.Extensions.Data
-B3.Extensions.Data includes data extensions for Entity Framework
+B3.Extensions.Data includes data extensions for Entity Framework Core
+## DbContextExtensions
+
+## ExecuteQuery
+
+Execute raw sql query without casting any type.
+```csharp
+public async Task<IActionResult> GetToplamMetraj()
+{
+	var sql = "SELECT type, SUM(length) FROM ways GROUP BY type";
+	var queryResult = await _context.ExecuteQueryAsync(sql);
+            
+    return Ok(response);
+}
+```
